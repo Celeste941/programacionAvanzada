736f6d253de3340be977d3dd46a66720c1d3704f
Celeste941 <anacelestep941@gmail.com>
2024-06-19 11:56:54 -0400

    se agrego throw error, para indiccar cuando hay un error en el llamado de la api

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,10 +4,14 @@ export const obtenerAnimales = async () => {
     try {
       const response = await fetch(llamarJson);
       const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error('Hay un error en la llamada de la api ');
+      }
       
       return data.animales;
     } catch (error) {
-      console.error("Error fetching animal data:", error);
+      console.error("Hay un error en la llamada de la api ", error);
     }
   };
   
